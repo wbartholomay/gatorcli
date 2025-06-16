a58fee780617a36af0490675235ba6defd46e076
wbartholomay <willbartholomay@gmail.com>
2025-06-15 20:46:23 -0400

    added posts table

---
+++ sql/schema/006_posts.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+CREATE TABLE posts(
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    description TEXT,
+    published_at TIMESTAMP,
+    feed_id UUID REFERENCES feed(id)
+        ON DELETE CASCADE,
+    UNIQUE(url)
+);
+
+-- +goose Down
+DROP TABLE posts;
