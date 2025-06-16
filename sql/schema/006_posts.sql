5e7d53881ec777fd0dc635bd9ae85a46fd05fb1d
wbartholomay <willbartholomay@gmail.com>
2025-06-15 20:57:26 -0400

    updated posts table and added posts queries

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -6,7 +6,7 @@ CREATE TABLE posts(
     title TEXT NOT NULL,
     url TEXT NOT NULL,
     description TEXT,
-    published_at TIMESTAMP,
+    published_at TIMESTAMP NOT NULL,
     feed_id UUID REFERENCES feed(id)
         ON DELETE CASCADE,
     UNIQUE(url)
